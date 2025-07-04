36b083e312cfe4fd816ae28167372e27c27ac95c
Syed Saqib Shah <syedsaqibshah@Syeds-MacBook-Air.local>
2025-07-04 19:39:50 +0500

    order api deleted - never used

--- src/app/api/orders/route.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
-
-export async function GET() {
-  const orders = await prisma.order.findMany();
-  return Response.json(orders);
-}
