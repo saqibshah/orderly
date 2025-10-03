017155d21bce64e376e86eb7393c812774773bb7
saqibshah <syedsaqibshah0011@gmail.com>
2025-10-03 16:14:50 +0500

    return page added

--- app/orders/returned/page.tsx
+++ app/orders/returned/page.tsx
@@ -1,7 +1,50 @@
+import { prisma } from "@/prisma/client";
+import { Flex, Table, Text } from "@radix-ui/themes";
+import { format } from "date-fns";
 import React from "react";
 
-const ReturnedOrders = () => {
-  return <div>ReturnedOrders</div>;
+const ReturnedOrders = async () => {
+  const orders = await prisma.order.findMany({
+    where: { status: "RETURNED" },
+  });
+
+  return (
+    <>
+      {orders.map((order) => (
+        <Table.Row key={order.id}>
+          <Table.Cell>
+            <Flex direction="column" gap="2">
+              <Text>{order.tracking}</Text>
+              <Text>{order.orderNumber}</Text>
+            </Flex>
+          </Table.Cell>
+          <Table.Cell>{order.courierStatus}</Table.Cell>
+          <Table.Cell>{order.status}</Table.Cell>
+          <Table.Cell>
+            <Flex direction="column" gap="2">
+              <Text>{order.customerName}</Text>
+              <Text>{order.address}</Text>
+            </Flex>
+          </Table.Cell>
+          <Table.Cell>
+            <Flex direction="column" gap="2">
+              <Text>{order.productOrdered}</Text>
+              <Text>{order.orderAmount}</Text>
+            </Flex>
+          </Table.Cell>
+          <Table.Cell>
+            {format(new Date(order.orderDate), "do MMM yyyy")}
+          </Table.Cell>
+          <Table.Cell>
+            {order.concludedAt
+              ? format(new Date(order.concludedAt), "do MMM yyyy, hh:mm a")
+              : ""}
+          </Table.Cell>
+          <Table.Cell>{order.remarks}</Table.Cell>
+        </Table.Row>
+      ))}
+    </>
+  );
 };
 
 export default ReturnedOrders;
