7abc4cc7f7419b4d1efed788ef5a2db6c73d1e99
saqibshah <syedsaqibshah0011@gmail.com>
2025-10-03 16:03:48 +0500

    added spinner and handled errors

--- app/verify-return/page.tsx
+++ app/verify-return/page.tsx
@@ -2,7 +2,9 @@
 
 import { Button, Callout, Container, TextField } from "@radix-ui/themes";
 import axios from "axios";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
+import Spinner from "../components/Spinner";
 
 interface VerifyReturn {
   tracking: string;
@@ -11,24 +13,49 @@ interface VerifyReturn {
 const VerifyReturnPage = () => {
   const { register, handleSubmit } = useForm<VerifyReturn>();
 
+  const [isReturned, setReturned] = useState("");
+  const [error, setError] = useState("");
+  const [isSubmitting, setSubmitting] = useState(false);
+
+  const onSubmit = handleSubmit(async (data) => {
+    try {
+      setSubmitting(true);
+      setError("");
+      setReturned("");
+      await axios.post("/api/verify_return", data);
+      setSubmitting(false);
+      setReturned(`${data.tracking} marked as returned`);
+    } catch (error) {
+      setSubmitting(false);
+      setError("An unexpected error occured.");
+      console.log(error);
+    }
+  });
+
   return (
-    <Container>
-      <form
-        className="max-w-xl space-y-3 p-5"
-        onSubmit={handleSubmit(async (data) => {
-          await axios.post("/api/verify_return", data);
-        })}
-      >
+    <Container className="p-5">
+      <div className="max-w-xl">
+        {error && (
+          <Callout.Root color="red" className="mb-5">
+            <Callout.Text>{error}</Callout.Text>
+          </Callout.Root>
+        )}
+      </div>
+      <form className="max-w-xl space-y-3 mb-5" onSubmit={onSubmit}>
         <TextField.Root
           placeholder="Tracking Number"
           {...register("tracking")}
         />
-        <Button>Submit</Button>
+        <Button disabled={isSubmitting}>
+          Submit {isSubmitting && <Spinner />}
+        </Button>
       </form>
-      <div className="max-w-xl space-y-3 p-5">
-        <Callout.Root color="blue">
-          <Callout.Text>Tracking Mark Returned.</Callout.Text>
-        </Callout.Root>
+      <div className="max-w-xl">
+        {isReturned && (
+          <Callout.Root color="blue">
+            <Callout.Text>{isReturned}</Callout.Text>
+          </Callout.Root>
+        )}
       </div>
     </Container>
   );
