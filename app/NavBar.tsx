f78a10c02eda459b070a5761af40917c4e6b9445
saqibshah <syedsaqibshah0011@gmail.com>
2025-10-03 16:34:05 +0500

    verify return button added to the navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React from "react";
 import classnames from "classnames";
+import { Button, Flex } from "@radix-ui/themes";
 
 const NavBar = () => {
   const currentPath = usePathname();
@@ -14,24 +15,52 @@ const NavBar = () => {
   ];
 
   return (
-    <nav className="flex space-x-6 border-b px-5 h-14 items-center">
-      <Link href="/">Logo</Link>
-      <ul className="flex space-x-6">
-        {links.map((link) => (
-          <Link
-            key={link.href}
-            className={classnames({
-              "text-zinc-900": link.href === currentPath,
-              "text-zinc-500": link.href !== currentPath,
-              "hover:text-zinc-800 transition-colors": true,
-            })}
-            href={link.href}
-          >
-            {link.label}
-          </Link>
-        ))}
-      </ul>
+    <nav className="border-b px-5 py-3">
+      <Flex justify="between" align="center">
+        <Flex align="center" gap="3">
+          <Link href="/">Logo</Link>
+          <ul className="flex space-x-6">
+            {links.map((link) => (
+              <Link
+                key={link.href}
+                className={classnames({
+                  "text-zinc-900": link.href === currentPath,
+                  "text-zinc-500": link.href !== currentPath,
+                  "hover:text-zinc-800 transition-colors": true,
+                })}
+                href={link.href}
+              >
+                {link.label}
+              </Link>
+            ))}
+          </ul>
+        </Flex>
+        <Flex align="center" gap="3">
+          <Button>
+            <Link href="/verify-return">Verify Return</Link>
+          </Button>
+        </Flex>
+      </Flex>
     </nav>
+
+    // <nav className="flex space-x-6 border-b px-5 h-14 items-center">
+    //   <Link href="/">Logo</Link>
+    //   <ul className="flex space-x-6">
+    //     {links.map((link) => (
+    //       <Link
+    //         key={link.href}
+    //         className={classnames({
+    //           "text-zinc-900": link.href === currentPath,
+    //           "text-zinc-500": link.href !== currentPath,
+    //           "hover:text-zinc-800 transition-colors": true,
+    //         })}
+    //         href={link.href}
+    //       >
+    //         {link.label}
+    //       </Link>
+    //     ))}
+    //   </ul>
+    // </nav>
   );
 };
 
